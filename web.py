69f7a55e202f4987c69edad0324c92e3f5347383
Bisu G <lannsnolo@gmail.com>
2025-08-05 04:49:30 +0545

    Update web.py

--- web.py
+++ web.py
@@ -1,14 +1,12 @@
 from flask import Flask
-import threading
 import os
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'bot is alive!'
+    return 'Bot is alive!'
 
 def run():
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 10000)))
-
-threading.Thread(target=run).start()  
+    port = int(os.environ.get('PORT', 10000))
+    app.run(host='0.0.0.0', port=port)
